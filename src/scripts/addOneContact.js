64692068df584e194d06d63090d9d7b46f614ed9
Maryna <tromsa3122@mail.com>
2025-05-29 17:16:38 +0300

    add

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,22 @@
-export const addOneContact = async () => {};
+import { readFile } from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  try {
+    const fileContent = await readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(fileContent || '[]');
+
+    const newContact = createFakeContact();
+    const updatedContacts = [...contacts, newContact];
+
+    await writeContacts(updatedContacts);
+
+    console.log('Один новий контакт успішно додано.');
+  } catch (error) {
+    console.error('Помилка при додаванні контакту:', error.message);
+  }
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+import { readFile } from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    const fileContent = await readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(fileContent || '[]');
+
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+
+    const updatedContacts = [...contacts, ...newContacts];
+    await writeContacts(updatedContacts);
+    console.log(`${number} нових контактів успішно додано.`);
+  } catch (error) {
+    console.error('Помилка під час генерації контактів:', error.message);
+  }
+};
 
 generateContacts(5);
